46880ef196d8dc627eaaff41d5bd5ba93130228b
Amitkumar <amitbhavikatte12@gmail.com>
2026-09-12 07:15:07 +0000

    Fix API URL for public deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://crack-edge-education-app.onrender.com/api",
 });
 
 export const auth = () => ({
